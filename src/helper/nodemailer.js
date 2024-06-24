6d9535dff873cbcd0d928960b483009f8752c4de
ikbar <kroniikbar@gmail.com>
2024-06-24 19:39:53 +0700

    refactor: improve email text verifikasi

--- src/helper/nodemailer.js
+++ src/helper/nodemailer.js
@@ -20,20 +20,31 @@ function sendTokenVerificationEmail(email, token) {
     subject: "Email Verification",
     html: `
     <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9;">
-      <h2 style="color: #333; text-align: center;">Verifikasi Email Anda</h2>
-      <p style="color: #555; text-align: center;">
-        Terima kasih telah mendaftar! Klik tombol di bawah untuk memverifikasi alamat email Anda.
+      <div style="margin-bottom: 20px;">
+        <img style="max-width: 200px;" src="https://i.ibb.co.com/nrN88dr/contrast-font-KHASs-1.png" alt="KHAVI App" />
+      </div>
+      <h2 style="color: #333; margin-bottom: 10px;">Verifikasi Email Anda</h2>
+      <p style="color: #555; margin-bottom: 20px;">
+        Hai! Terima kasih telah bergabung dengan KHAVI App. Silakan klik tombol di bawah untuk memverifikasi alamat email Anda.
       </p>
-      <div style="text-align: center; margin: 20px;">
-        <a href="http://localhost:${process.env.PORT}/api/auth/verify/${token}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">
+      <div style="margin-bottom: 20px;">
+        <a href="${process.env.DEPLOYMENT_URL}/api/auth/verify/${token}" style="background-color: #4caf50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block; transition: background-color 0.3s;">
           Verifikasi Email
         </a>
       </div>
-      <p style="color: #777; text-align: center; font-size: 12px;">
-        Jika Anda tidak mendaftar akun ini, harap abaikan email ini.
+      <p style="color: #777; font-size: 12px; margin-bottom: 10px;">
+        Jika Anda tidak mendaftar untuk akun ini, silakan abaikan email ini.
       </p>
+      <p style="color: #777; font-size: 12px; margin-bottom: 20px;">
+        Terima kasih! 🌟 Dikirimkan oleh Ikbar dari tim KHAVI App
+      </p>
+      <blockquote style="margin: 0;">
+        <p style="color: #777; font-size: 12px; font-style: italic;">
+          "Mimpi itu gratis, tapi kelelahan menjalankannya adalah berbayar." - Anonymous
+        </p>
+      </blockquote>
     </div>
-  `,
+    `,
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
